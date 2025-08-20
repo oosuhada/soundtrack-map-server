fa924f68c4a3d4b3b07fa5420a69cecd2e1d04a4
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-08-20 22:57:11 +0900

    feat(core): implement ItemLikeRepository logic

---
+++ backend/streetdrop-api/src/main/java/com/depromeet/domains/item/repository/ItemLikeRepository.java
@@ -0,0 +1,15 @@
+package com.depromeet.domains.item.repository;
+
+import com.depromeet.item.ItemLike;
+import com.depromeet.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemLikeRepository extends JpaRepository<ItemLike, Long>, QueryDslItemLikeRepository {
+    Optional<ItemLike> findByItemIdAndUser(Long itemId, User user);
+    boolean existsByUserIdAndItemId(Long userId, Long itemId);
+    int countByItemId(Long itemId);
+}
