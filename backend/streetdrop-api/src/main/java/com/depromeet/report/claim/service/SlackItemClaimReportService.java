e14ab26ff66eec662a9071c10ea6a89814dd749a
oosuhada <185910926+oosuhada@users.noreply.github.com>
2025-09-16 20:23:38 +0900

    feat(core): implement SlackItemClaimReportService logic

---
+++ backend/streetdrop-api/src/main/java/com/depromeet/report/claim/service/SlackItemClaimReportService.java
@@ -0,0 +1,44 @@
+package com.depromeet.report.claim.service;
+
+import com.depromeet.external.slack.SlackService;
+import com.depromeet.report.claim.dto.ItemClaimReportDto;
+import com.depromeet.report.claim.service.ItemClaimReportService;
+import com.slack.api.model.block.LayoutBlock;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+
+@Component
+@RequiredArgsConstructor
+public class SlackItemClaimReportService implements ItemClaimReportService {
+
+    private final SlackService slackService;
+    @Value("${slack.report-channel}")
+    private String slackReportingChannel;
+    private static final String TITLE = "Item Claim Report";
+
+    public void sendReport(ItemClaimReportDto itemClaimReportDto) {
+        var markdownTextObject = markdownText("*Item Claim Id:* " + itemClaimReportDto.itemClaimId()
+                + "\n*Item Claim Reason:* " + itemClaimReportDto.itemClaimReason()
+                + "\n*Item Claim Status:* " + itemClaimReportDto.itemClaimStatus()
+                + "\n*Reporter:* " + itemClaimReportDto.reportUserId()
+                + "\n*Item Id:* " + itemClaimReportDto.itemId()
+                + "\n*Item Content:* " + itemClaimReportDto.itemContent()
+                + "\n*Claim Time:* " + itemClaimReportDto.claimTime()
+        );
+
+        List<LayoutBlock> blocks = asBlocks(
+                header(h -> h.text(plainText(TITLE))),
+                section(s -> s.text(markdownTextObject))
+        );
+
+        slackService.sendMessage(blocks, slackReportingChannel);
+    }
+
+}
